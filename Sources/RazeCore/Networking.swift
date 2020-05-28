3e984285de21e477c8a20584bdfc9bc90b5aa1c3
Bruce Wade <bruce.wade@gmail.com>
2020-05-28 08:38:22 -0700

    Added Networking class

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -1,5 +1,5 @@
 //
-//  RazeCore.swift
+//  Networking.swift
 //  RazeCore
 //
 //  Created by Bruce Wade on 2020-05-28.
@@ -7,6 +7,9 @@
 
 import Foundation
 
-public struct RazeCore {
+extension RazeCore {
     
+    public class Networking {
+        
+    }
 }
